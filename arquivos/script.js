d95d60cbe636c3167e760711e5c943f7cf3a5c61
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-07-01 14:20:26 -0300

    finalizado-css/js
    ultimas atualizações no CSS e adicionado as duas funções no JS

---
+++ arquivos/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything; 
+}
+
+function trocarCirculo (color) {
+    const circulo = document.querySelector(".circulo")
+    circulo.style.background = color;
+}
